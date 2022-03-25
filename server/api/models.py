5aea9ca0fc8c9dad4f1ad905f5cb7a3d0fe17768
HyeonjaeKwon <href.hjk@gmail.com>
2022-03-25 19:45:06 +0900

    ADD - 모델 생성

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Test(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+
+
+class Question(models.Model):
+    test = models.ForeignKey(Test, related_name='questions',
+                             on_delete=models.CASCADE)
+    question = models.CharField(max_length=256)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question, related_name='answers', on_delete=models.CASCADE)
+    answer = models.CharField(max_length=64)
+    score = models.IntegerField()
+
+
+class Result(models.Model):
+    test = models.ForeignKey(Test, related_name='results',
+                             on_delete=models.CASCADE)
+    result = models.CharField(max_length=256)
+    lower = models.IntegerField()
+    upper = models.IntegerField()
