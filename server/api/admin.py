c2701941b47da41b6fffb655a98868d351c2b12a
HyeonjaeKwon <href.hjk@gmail.com>
2022-04-01 14:01:09 +0900

    ADD - admin 페이지 모델 추가

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Test, Question, Answer, Result
+
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Result)
