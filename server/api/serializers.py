bde62f9569794f3b08d06c593cc8a6a78c8a45d1
HyeonjaeKwon <href.hjk@gmail.com>
2022-04-01 13:43:23 +0900

    FEAT - serializers 구현

---
+++ server/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from api.models import Test, Question, Answer, Result
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        fields = ('result', 'lower', 'upper')
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('answer', 'score')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ('question')
+
+
+class TestListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ('id', 'name', 'description', 'create_date')
+
+
+class TestSerializer(serializers.ModelSerializer):
+    content = QuestionSerializer(many=True, read_only=True, source='questions')
+    result = ResultSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Test
+        fields = ('id', 'contents', 'results')
