29ffbb56dfff270a814dbb6b26af680a08369c17
HyeonjaeKwon <href.hjk@gmail.com>
2022-04-28 14:02:30 +0900

    FEAT - nested admin 구현

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+import nested_admin
 
 from api.models import Test, Question, Answer, Result
 
-admin.site.register(Test)
+
+class ResultInline(nested_admin.NestedStackedInline):
+    model = Result
+    extra = 1
+
+
+class AnswerInline(nested_admin.NestedStackedInline):
+    model = Answer
+    extra = 1
+
+
+class QuestionInline(nested_admin.NestedStackedInline):
+    model = Question
+    extra = 1
+    inlines = [AnswerInline]
+
+
+class TestAdmin(nested_admin.NestedModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'description', 'create_date']})
+    ]
+    inlines = [QuestionInline, ResultInline]
+
+
+admin.site.register(Test, TestAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Result)
--- server/config/settings.py
+++ server/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'nested_admin',
     'api.apps.ApiConfig',
 ]
 
