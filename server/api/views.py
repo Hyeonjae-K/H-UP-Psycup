ddcaa17adc679dc475d09f5fc41e4580763a6f1e
HyeonjaeKwon <href.hjk@gmail.com>
2022-05-01 13:35:32 +0900

    FIX - viewsets.py를 views.py로 이전

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+from api.models import Test
+from api.serializers import TestSerializer, ContentSerializer
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def test_list(request):
+    tests = Test.objects.all()
+    serializer = TestSerializer(tests, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def test_detail(request, pk):
+    test = Test.objects.get(pk=pk)
+    serializer = ContentSerializer(test)
+    return Response(serializer.data)
