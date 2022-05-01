fa576c35934e708a2f92d49fa871b68ff22efd5c
HyeonjaeKwon <href.hjk@gmail.com>
2022-05-01 13:35:58 +0900

    FIX - router 삭제 및 url 수정

--- server/config/routers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-
-from api.viewsets import TestViewSet, ContentViewSet
-
-router = routers.DefaultRouter()
-router.register(r'tests', TestViewSet, 'tests')
-router.register(r'contents', ContentViewSet, 'contents')
--- server/config/urls.py
+++ server/config/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from config.routers import router
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/test', views.test_list),
+    path('api/test/<int:pk>', views.test_detail),
 ]
