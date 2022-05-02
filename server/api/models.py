c9406ddc8ca19dcec93812c35a44e39c9254de1d
Your Name <you@example.com>
2022-05-02 16:58:22 +0100

    FIX - 필드 옵션 수정 및 str 매직 메소드 반환 타입 지정

--- server/api/models.py
+++ server/api/models.py
@@ -5,44 +5,45 @@ from django.utils import timezone
 class Test(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField()
-    thumbnail = models.ImageField(upload_to='thumbnails/')
+    thumbnail = models.ImageField(
+        upload_to='thumbnails/')
     create_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Question(models.Model):
     test = models.ForeignKey(Test, related_name='questions',
                              on_delete=models.CASCADE)
     image = models.ImageField(
-        upload_to='question_images/', null=True)
+        upload_to='question_images/', null=True, blank=True)
     question = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.question
+        return str(self.question)
 
 
 class Answer(models.Model):
     question = models.ForeignKey(
         Question, related_name='answers', on_delete=models.CASCADE)
     image = models.ImageField(
-        upload_to='answer_images/', null=True)
-    answer = models.CharField(max_length=64, null=True)
+        upload_to='answer_images/', null=True, blank=True)
+    answer = models.CharField(max_length=64, null=True, blank=True)
     score = models.IntegerField()
 
     def __str__(self):
-        return self.answer
+        return str(self.answer)
 
 
 class Result(models.Model):
     test = models.ForeignKey(Test, related_name='results',
                              on_delete=models.CASCADE)
     image = models.ImageField(
-        upload_to='result_images/', null=True)
+        upload_to='result_images/', null=True, blank=True)
     result = models.CharField(max_length=256)
     lower = models.IntegerField()
     upper = models.IntegerField()
 
     def __str__(self):
-        return self.result
+        return str(self.result)
